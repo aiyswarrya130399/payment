ef00237579ecff2a35fa0880933eb680aa89f867
aiyswarrya130399 <aiyswarrya130399@users.noreply.github.com>
2026-01-14 17:45:26 +0000

    ci: publish to gh-pages (via PAT) 6b200be426f7ec772d64f68fb1efa07858a3ea72

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Auto-generated during CI. Do not commit your real config.js with secrets.
 const CONFIG = {
-  MERCHANT_UPI_ID: "9940014468@ybl",
-  MERCHANT_NAME: "aravind"
+  MERCHANT_UPI_ID: "swotifyca@ybl",
+  MERCHANT_NAME: "AiyswarryaMohan"
 };
